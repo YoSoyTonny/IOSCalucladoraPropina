e0eeb9d35b628f4815a6c61288e5e06c757b3749
Tonny Gammer <tonnygammer@macbook-pro-de-tonny.local>
2019-08-07 14:21:26 -0700

    Initial Commit

---
+++ Practica 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica 1
+//
+//  Created by Tonny Gammer on 07/08/19.
+//  Copyright © 2019 Tonny Gammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
